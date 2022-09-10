848e285ff7cb7a4b4eea544251c2bbba37a552b0
Ivan Windon <ivan.windon@icloud.com>
2022-09-10 16:53:46 -0400

    Initial Commit

---
+++ IBBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBBasic
+//
+//  Created by Ivan Windon on 9/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
